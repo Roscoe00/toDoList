e77a9c8eae7c7bb936eda39fe6d503464a19c707
Roscoe Bull <roscoe-bull@live.co.uk>
2021-11-24 15:45:58 +0000

    got input appearing with checkbox and edit icon after pressing add

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 const inputText = document.querySelector("#list__text")
 const inputButton = document.querySelector("#list__add")
 
-const resetButton = document.querySelector("#reset")
-const resetButton = document.querySelector("#delete")
+const outputText = document.querySelector("#list__output")
+
+// const resetButton = document.querySelector("#reset")
+// const deleteButton = document.querySelector("#delete")
+
+inputButton.addEventListener("click",()=>{
+   return outputText.innerHTML += 
+   `<div class="task">
+      <input type="checkbox" class="select">
+      <span class="task__log">${inputText.value}</span>
+      <span class="task__edit">&#9998</span>
+   </div>`;
+})
