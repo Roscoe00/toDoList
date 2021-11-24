b1e3e9c76a7ff3729bbffd4202b24c4bca6cff79
Roscoe Bull <roscoe-bull@live.co.uk>
2021-11-24 16:27:04 +0000

    reset button working

--- main.js
+++ main.js
@@ -3,14 +3,22 @@ const inputButton = document.querySelector("#list__add")
 
 const outputText = document.querySelector("#list__output")
 
-// const resetButton = document.querySelector("#reset")
+const resetButton = document.querySelector("#reset")
 // const deleteButton = document.querySelector("#delete")
 
+listNumber = 0
+
 inputButton.addEventListener("click",()=>{
+   listNumber=listNumber+1
    return outputText.innerHTML += 
-   `<div class="task">
+   `<div class="task" id="task__${listNumber}">
       <input type="checkbox" class="select">
       <span class="task__log">${inputText.value}</span>
       <span class="task__edit">&#9998</span>
    </div>`;
+})
+
+resetButton.addEventListener("click",()=>{
+    listNumber=0
+    return outputText.innerHTML =  `<div id="list__output"></div>`
 })
