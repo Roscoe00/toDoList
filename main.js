e935e4185990ea65bc3ebbf9f39782f9816c10b4
Roscoe Bull <roscoe-bull@live.co.uk>
2021-11-24 15:05:01 +0000

    basic styling finished, start of js

---
+++ main.js
@@ -0,0 +1,5 @@
+const inputText = document.querySelector("#list__text")
+const inputButton = document.querySelector("#list__add")
+
+const resetButton = document.querySelector("#reset")
+const resetButton = document.querySelector("#delete")
